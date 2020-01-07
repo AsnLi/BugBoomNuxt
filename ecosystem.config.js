d9574667080e6a9329b99772e9b5763abe718b53
AsnLi <18390955482@163.com>
2020-01-07 17:40:25 +0800

    init pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,19 @@
 module.exports = {
+  // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
   apps : [{
-    name: 'jiaxiaoclass',
-    script: './server/index.js',
-    cwd: __dirname,
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
+    name                : 'jiaxiaoclass',
+    script              : './server/index.js',
+    cwd                 : __dirname,
+    args                : '',
+    instances           : 1,
+    'error_file'        : './logs/pm2/error.log',
+    'out_file'          : './logs/pm2/out.log',
+    'pid_file'          : './pids/pid.pid',
+    'merge_logs'        : false,
+    'ignore_watch'      : ['node_modules', 'logs', 'pids'],
+    autorestart         : true,
+    watch               : false,
+    max_memory_restart  : '1G',
     env: {
       NODE_ENV: 'development'
     },
@@ -22,16 +27,16 @@ module.exports = {
   */
   deploy : {
     production : {
-      user : 'root', // Nginx服务器上的username
-      host : '139.224.118.112',
-      port : "22",
-      ref  : 'origin/master',
-      repo : 'git@github.com:AsnLi/BugBoomNuxt.git',
-      path : '/var/www/production',
-      ssh_options: 'StrictHostKeyChecking=no',
+      user          : 'root', // Nginx服务器上的username
+      host          : ['139.224.118.112'],
+      port          : '22',
+      ref           : 'origin/master',
+      repo          : 'git@github.com:AsnLi/BugBoomNuxt.git',
+      path          : '/var/www/production',
+      ssh_options   : 'StrictHostKeyChecking=no',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js -- run start',
       env: {
-        "NODE_ENV": "production"
+        'NODE_ENV': 'production'
       }
     }
   }
